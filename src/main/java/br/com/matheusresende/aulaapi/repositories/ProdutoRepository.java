992a8cec355e430bb353f44f5a982fbda9fa8089
Aluno <aluno@eteclencois.com.br>
2019-05-08 08:54:49 -0300

    Concerto da Api

--- src/main/java/br/com/matheusresende/aulaapi/repositories/ProdutoRepository.java
+++ src/main/java/br/com/matheusresende/aulaapi/repositories/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package br.com.matheusresende.aulaapi.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import br.com.matheusresende.aulaapi.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	public Page<Produto> findByNomeContaining(String nome, Pageable pageable);
 
 }
